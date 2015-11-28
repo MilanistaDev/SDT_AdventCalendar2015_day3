d8fed060a9c592edf3c21c208e4e55c36bdeb1ad
Takuya Aso <milanista224@gmail.com>
2015-11-29 03:17:29 +0900

    Initial Commit

---
+++ UIViewHierarchyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIViewHierarchyTest
+//
+//  Created by 麻生 拓弥 on 2015/11/29.
+//  Copyright © 2015年 麻生 拓弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
